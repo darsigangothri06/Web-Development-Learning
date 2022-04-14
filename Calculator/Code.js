8d0a686adf0e766c633a40e450bfa392855c6318
darsigangothri06 <darsigangothri@gmail.com>
2022-04-14 22:11:26 +0530

    Add: Calculator

---
+++ Calculator/Code.js
@@ -0,0 +1,23 @@
+// binding variables
+let num9 = document.getElementById("num9")
+let num8 = document.getElementById("num8")
+let num7 = document.getElementById("num7")
+let num6 = document.getElementById("num6")
+let num5 = document.getElementById("num5")
+let num4 = document.getElementById("num4")
+let num3 = document.getElementById("num3")
+let num2 = document.getElementById("num2")
+let num1 = document.getElementById("num1")
+let num0 = document.getElementById("num0")
+let add = document.getElementById("add")
+let subtract = document.getElementById("subtract")
+let multiply = document.getElementById("multiply")
+let division = document.getElementById("division")
+let result = document.getElementById("result")
+let point = document.getElementById("point")
+
+var operand1 = null
+
+function evaluate() {
+
+}
